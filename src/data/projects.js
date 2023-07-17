38788c55308cd0fb2da0a04f41eab215750f2337
HotBarbeque <122079759+HotBarbeque@users.noreply.github.com>
2023-07-17 18:03:00 -0500

    Finished up projects data

---
+++ src/data/projects.js
@@ -0,0 +1,28 @@
+const data = [
+  {
+    title: 'Movie Match Maker',
+    subtitle: 'Full Stack Bootcamp Group 1 Project',
+    link: 'https://adamhood15.github.io/movie-match-maker/',
+    image: '/images/projects/MovieMatchMaker.png',
+    date: '2023-04-10',
+    desc: 'This is an application to select a movie for someone.',
+  },
+  {
+    title: 'BookTrackr',
+    subtitle: 'Full Stack Bootcamp Group 2 Project',
+    link: 'https://bookmanagement.herokuapp.com/',
+    image: '/images/projects/BookTrackr.png',
+    date: '2023-06-6',
+    desc: 'Book Management System allows users to keep track of their personal book collection. The application will provide a user-friendly interface for managing books, including adding new books, updating book details, and viewing the book collection.',
+  },
+  {
+    title: 'Project 3',
+    subtitle: 'Full Stack Bootcamp Group 3 Project.',
+    link: '',
+    image: '/images/projects/ComingSoon.png',
+    date: '2023-08-8',
+    desc: 'Coming Soon',
+  },
+];
+
+export default data;
