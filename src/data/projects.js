fbaec212607697d025525f32267cbea6f3d29f21
HotBarbeque <122079759+HotBarbeque@users.noreply.github.com>
2023-07-18 19:06:39 -0500

    Added more projects to reach required amount

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,7 +12,7 @@ const data = [
     subtitle: 'Full Stack Bootcamp Group 2 Project',
     link: 'https://bookmanagement.herokuapp.com/',
     image: '/images/projects/BookTrackr.png',
-    date: '2023-06-6',
+    date: '2023-06-06',
     desc: 'Book Management System allows users to keep track of their personal book collection. The application will provide a user-friendly interface for managing books, including adding new books, updating book details, and viewing the book collection.',
   },
   {
@@ -20,8 +20,32 @@ const data = [
     subtitle: 'Full Stack Bootcamp Group 3 Project.',
     link: '',
     image: '/images/projects/ComingSoon.png',
-    date: '2023-08-8',
+    date: '2023-08-08',
     desc: 'Coming Soon',
+  },
+  {
+    title: 'Tech Blog',
+    subtitle: 'Full Stack Bootcamp Challenge.',
+    link: 'https://c14-tech-blog-kc.herokuapp.com/',
+    image: '/images/projects/techblog.png',
+    date: '2023-05-30',
+    desc: 'Full Stack web application for tech blog posts. The app allows a user to sign-up, login, add and view comments in a blog setting.',
+  },
+  {
+    title: 'Progressive Web App: Text Editor',
+    subtitle: 'Full Stack Bootcamp PWA Challenge',
+    link: 'https://c19-pwa-text-editor-kc-5cccbca85235.herokuapp.com/',
+    image: '/images/projects/JATE.png',
+    date: '2023-07-11',
+    desc: 'A single-page text editor that meets the PWA criteria, runs in the browser, and functions offline. It features a number of data persistence techniques that serve as redundancy in case one of the options is not supported by the browser.',
+  },
+  {
+    title: 'Note Taker',
+    subtitle: 'Full Stack Bootcamp Challenge.',
+    link: 'https://c11-notetaker-kc.herokuapp.com/',
+    image: '/images/projects/notetaker.png',
+    date: '2023-05-08',
+    desc: 'An application called Note Taker that can be used to write and save notes. Utilizes express.js.',
   },
 ];
 
